f6ed64a337138dfd7c5940e9f8d9afbd502be53e
Yuqiao Wang <allenwyq997@gmail.com>
2024-06-05 02:30:50 +0900

    0604 结果汇总

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import json
 # 50以下为下等马
 # 其余为中等马
 kPowerThreshold = 55
-kNormalThreshold = 46
+kNormalThreshold = 45
 
 def team_addition(team, team_weight, team_positions,
                   weight, lane, player_name, player_data):
