928a52ffbc397d39592bacfd47b0237822982b20
Yuqiao Wang <allenwyq997@gmail.com>
2025-04-11 14:32:35 +0900

    s3

--- main.py
+++ main.py
@@ -410,7 +410,7 @@ def update_player_stats(players, player_name, is_winner):
     player_data["loss"] = loses
 
 
-def load_players_data(filename="october.json"):
+def load_players_data(filename="S3_stats.json"):
     try:
         with open(filename, "r", encoding="utf-8") as file:
             players = json.load(file)
@@ -420,7 +420,7 @@ def load_players_data(filename="october.json"):
         return None
 
 
-def save_players_data(players, filename="october.json"):
+def save_players_data(players, filename="S3_stats.json"):
     with open(filename, "w", encoding="utf-8") as file:
         json.dump(players, file, ensure_ascii=False, indent=4)
 
